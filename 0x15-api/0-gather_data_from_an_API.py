5af9c899e57743de15c06524e0e89438f0748e21
Kitimbo Rino <kitimborinoemma0580@gmail.com>
2024-04-22 18:31:53 +0300

    fix: 0. Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,34 +3,25 @@
 Python script that returns information using REST API
 '''
 import requests
-import sys
-
-
-def get_employee_todo_progress(employee_id):
-    # API endpoint
-    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-
-    # Fetching data from the API
-    response = requests.get(url)
-    todos = response.json()
-
-    # Extracting relevant information
-    employee_name = todos[0]['name'].split()[0]
-    total_tasks = len(todos)
-    done_tasks = [todo for todo in todos if todo['completed']]
-    num_done_tasks = len(done_tasks)
-
-    # Displaying information
-    print(f"Employee {employee_name} is done with tasks({
-        num_done_tasks}/{total_tasks}): ")
-    for task in done_tasks:
-        print(f"\t{task['title']}")
-
+from sys import argv
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py EMPLOYEE_ID")
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
+    if len(argv) > 1:
+        user = argv[1]
+        url = "https://jsonplaceholder.typicode.com/"
+        req = requests.get("{}users/{}".format(url, user))
+        name = req.json().get("name")
+        if name is not None:
+            jreq = requests.get(
+                "{}todos?userId={}".format(
+                    url, user)).json()
+            alltsk = len(jreq)
+            completedtsk = []
+            for t in jreq:
+                if t.get("completed") is True:
+                    completedtsk.append(t)
+            count = len(completedtsk)
+            print("Employee {} is done with tasks({}/{}):"
+                  .format(name, count, alltsk))
+            for title in completedtsk:
+                print("\t {}".format(title.get("title")))
